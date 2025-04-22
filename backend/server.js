47f339c529cddea0513d73e296510522b6f44d68
Md-Abdullah1 <abdullahahil153@gmail.com>
2025-04-22 23:53:33 +0530

    added schemas

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,5 @@ const app = express();
 app.use(express.json());
 const PORT = 4000;
 app.listen(PORT, () => {
-    console.log(`Server is running on  : http://localhost:${PORT}`);
-    
+  console.log(`Server is running on  : http://localhost:${PORT}`);
 });
