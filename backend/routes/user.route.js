9094111356ff4b277df3746456d3adda915eedb3
Md-Abdullah1 <abdullahahil153@gmail.com>
2025-05-12 19:35:31 +0530

    updated registratin logic

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { registerUser, loginUser, getUserProfile } = require('../controllers/user.controller');
 const protect = require('../middlewares/auth.middleware');
+const checkRegister = require('../middlewares/register.middleware')
 
 const router = express.Router();
-router.route('/register').post(registerUser);
+router.route('/register').post(checkRegister,registerUser);
 router.route('/login').post(loginUser);
 router.route('/me',protect,getUserProfile)
 
