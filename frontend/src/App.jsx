344c241f1ff8ce4cfda6791ce2a2181718b8d9d9
Md-Abdullah1 <abdullahahil153@gmail.com>
2025-05-20 00:03:08 +0530

    setup completed for tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import Home from "./views/pages/home";
 
 function App() {
   return (
-    <div className="app">
+    <div className="app bg-amber-500 w-screen h-screen">
       <BrowserRouter>
         {/* <Routes>{allRoutes?.map}</Routes> */}
         <Routes><Route path={`/`} element={<Home/>}></Route></Routes>
